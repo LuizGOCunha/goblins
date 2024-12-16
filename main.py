7510d5db703d8d6c317260aca9ef06c78f4b789f
Luiz <luiz.oliveira@thoughtful.ai>
2024-12-16 18:22:40 -0300

    Added more tests on main module

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
     from time import sleep
     g1 = Goblin("1")
     g2 = Goblin("2", 9, 9)
+    g3 = Goblin("3", 7, 7)
     while True:
         g1.act()
         g2.act()
+        g3.act()
         display_grid()
         sleep(1)
